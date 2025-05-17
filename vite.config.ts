4bf446e4ea95d623a6015d78d6ae024af859cc63
liamvnastoria <liam.dispa@skygenesisenterprise.com>
2025-05-18 01:48:36 +0200

    Update licence and vite

--- vite.config.ts
+++ vite.config.ts
@@ -6,13 +6,13 @@ export default defineConfig({
     plugins: [
         react(),
         {
-            name: "custom-message", // Nom du plugin
+            name: "custom-message",
             configureServer(server) {
                 console.log("\x1b[36m%s\x1b[0m", "🚀 Vite server is starting..."); // Message personnalisé en cyan
-                console.log("\x1b[32m%s\x1b[0m", "🌟 Aether Mail Frontend is ready to go!");
+                console.log("\x1b[32m%s\x1b[0m", "🌟 IoT Platform Frontend is ready to go!");
             },
             buildStart() {
-                console.log("\x1b[33m%s\x1b[0m", "📦 Building Aether Mail Frontend...");
+                console.log("\x1b[33m%s\x1b[0m", "📦 Building IoT Platform Frontend...");
             },
         },
     ],
